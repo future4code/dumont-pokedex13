9a045f6c3fa1a994dbcb76f44d9f7ba2f6d9ad6f
Fábio <fabiojordao97@yahoo.com.br>
2020-11-23 16:46:09 -0300

    Posicionamento dos componentes visuais do site.

--- pokedex/src/components/details.js
+++ pokedex/src/components/details.js
@@ -1,11 +1,40 @@
 import React from "react"
 import styled from 'styled-components'
 import axios from 'axios'
+import { DivButton, GridPokemon, H1Home, Header, PokemonBox, HeaderDetails, DetailButtons, DetailsContainer, DetailsPictures, StyledPic } from "../styled/styles"
+import { useHistory } from "react-router-dom"
+import {goToPokedex, goBack} from '../router/coordinations'
+import pikachu from '../components/img/pikachu.png'
 
-function Details () {
+function Details() {
+
+    const history = useHistory()
+
     return (
+
+
         <div>
-            Página de Detalhes
+
+            <HeaderDetails>
+
+                <DetailButtons onClick={() => goToPokedex(history)}>Ir para Pokedéx</DetailButtons>
+
+
+                <h1>Lista de Pokémons</h1>
+
+                <DetailButtons onClick={() => goBack(history)}>Voltar para Home</DetailButtons>
+            </HeaderDetails>
+
+            <DetailsContainer>
+            <div>
+                <DetailsPictures>
+                <StyledPic src={pikachu} />
+                <img src={pikachu} />
+                </DetailsPictures>
+                <div>Poderes</div>
+                <div>Principais Ataques</div>
+            </div>
+            </DetailsContainer>
         </div>
     )
 }
