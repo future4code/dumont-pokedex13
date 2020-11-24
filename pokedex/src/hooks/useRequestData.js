b7de594148820cbb5a46de7d86895c4e66945249
Fábio <fabiojordao97@yahoo.com.br>
2020-11-24 16:56:14 -0300

    Tentativa falha do map da home.

---
+++ pokedex/src/hooks/useRequestData.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export function useRequestData(url, initialState) {
+
+   const [data, setData] = useState(initialState)
+   
+   useEffect(()=> {
+       axios.get(url)
+       .then((response) => {
+           setData(response.data.sprites)
+       })
+       .catch((error) => {
+           console.log(error)
+       })
+   }, [url])
+   return data
+}
